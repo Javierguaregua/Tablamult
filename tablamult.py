882538f216d5af1e2473c67eee6e0ca17fe1be5f
Javierguaregua <40311049+Javierguaregua@users.noreply.github.com>
2018-06-16 00:54:16 -0400

    Add files via upload

---
+++ tablamult.py
@@ -0,0 +1,19 @@
+print ("Tabla de Multiplicar \nJavier Guaregua - 2018")
+print ("")
+print ("Coloca un numero y obten los resultados de su tabla de multiplicar")
+print("")
+print("Ojo: Solo colocar numeros, de lo contrario fallara")
+
+while True:
+    num = float(input("\nEscribe aqui el numero u la cantidad:\n"))
+    print(num, "por 1 =",num*1)
+    print(num, "por 2 =",num*2)
+    print(num, "por 3 =",num*3)
+    print(num, "por 4 =",num*4)
+    print(num, "por 5 =",num*5)
+    print(num, "por 6 =",num*6)
+    print(num, "por 7 =",num*7)
+    print(num, "por 8 =",num*8)
+    print(num, "por 9 =",num*9)
+    print(num, "por 10 =",num*10)
+
